e198114d79b562f51445c94fe2e595710e34668a
bibash77261081 <cbibash20@gmail.com>
2023-05-18 00:51:24 +0545

    login logic created

--- app/src/main/java/com/example/ecochoice/LoginActivity.java
+++ app/src/main/java/com/example/ecochoice/LoginActivity.java
@@ -1,14 +1,105 @@
 package com.example.ecochoice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    ProgressBar progressBar;
+    Button btnLogin;
+    TextView txtNewUser;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        btnLogin = findViewById(R.id.btn_login);
+        txtNewUser = findViewById(R.id.txtNewUser);
+        mAuth = FirebaseAuth.getInstance();
+
+        txtNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    editTextEmail.setError("Email can't be empty!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    editTextPassword.setError("Password can't be empty!");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(LoginActivity.this, "Logged in successfully.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecochoice/RegisterActivity.java
+++ app/src/main/java/com/example/ecochoice/RegisterActivity.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,6 +26,18 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
     TextView txtRegistered;
     FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +64,6 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
                 String email, password, confirmPassword;
 
                 email = String.valueOf(editTextEmail.getText());
@@ -64,19 +76,21 @@ public class RegisterActivity extends AppCompatActivity {
                 }
 
                 if(TextUtils.isEmpty(password)){
-                    editTextEmail.setError("Password can't be empty!");
+                    editTextPassword.setError("Password can't be empty!");
                     return;
                 }
 
                 if(TextUtils.isEmpty(confirmPassword)){
-                    editTextEmail.setError("Please confirm password!");
+                    editTextConfirmPassword.setError("Please confirm password!");
                     return;
                 }
 
                 if(!confirmPassword.equals(password)){
-                    editTextEmail.setError("Password confirmation does not match!");
+                    editTextConfirmPassword.setError("Password confirmation does not match!");
                     return;
                 }
+
+                progressBar.setVisibility(View.VISIBLE);
 
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
