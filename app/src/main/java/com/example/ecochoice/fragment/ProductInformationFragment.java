9d0ebe3efd33835c5cd8da02f79ce3f402a51c2e
bibash77261081 <cbibash20@gmail.com>
2023-06-06 02:07:17 +0545

    delete product

--- app/src/main/java/com/example/ecochoice/fragment/ProductInformationFragment.java
+++ app/src/main/java/com/example/ecochoice/fragment/ProductInformationFragment.java
@@ -4,16 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.example.ecochoice.MainActivity;
 import com.example.ecochoice.R;
 import com.example.ecochoice.model.Product;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 
@@ -26,6 +37,8 @@ public class ProductInformationFragment extends Fragment {
     private TextView environmentalEffectTextView;
     private TextView ecoFriendlyTipsTextView;
     private TextView alternativeProductsTextView;
+    private Button deleteProductButton;
+    private DatabaseReference databaseReference;
 
     private Product product;
 
@@ -63,6 +76,52 @@ public class ProductInformationFragment extends Fragment {
         environmentalEffectTextView = view.findViewById(R.id.environmentalEffectTextView);
         ecoFriendlyTipsTextView = view.findViewById(R.id.ecoFriendlyTipsTextView);
         alternativeProductsTextView = view.findViewById(R.id.alternativeProductsTextView);
+        deleteProductButton = view.findViewById(R.id.deleteProductButton);
+        databaseReference = FirebaseDatabase.getInstance().getReference("products");
+
+        deleteProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (product != null) {
+                    String productName = product.getName();
+                    String productDescription = product.getDescription();
+
+                    // Find the product in the database based on name and description
+                    Query query = databaseReference.orderByChild("name").equalTo(productName)
+                            .orderByChild("description").equalTo(productDescription);
+
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                snapshot.getRef().removeValue()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                // Deletion successful
+                                                Toast.makeText(requireContext(), "Product deleted", Toast.LENGTH_SHORT).show();
+                                                // Optionally, navigate back or perform other actions
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Deletion failed
+                                                Toast.makeText(requireContext(), "Failed to delete product", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                break; // Assuming the product is unique based on name and description
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Error occurred while querying the database
+                            Toast.makeText(requireContext(), "Failed to delete product", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
 
         populateProductInformation();
     }
